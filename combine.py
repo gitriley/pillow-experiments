1a1ba9f111539a88dc1db4220223cf92ecfb97aa
Riley <20868615+gitriley@users.noreply.github.com>
2018-04-15 21:58:01 -0700

    cleaned up file

--- combine.py
+++ combine.py
@@ -1,21 +1,6 @@
 from PIL import Image
 import os
 import sys
-'''
-truths/variables...
-
-newImage size...(e.g. 1500,1500)...should be 2x the smallest image, then cropped to square?
-inputImage1 size
-inputImage2 size
-should the splice bars be vertical or horizontal 
-slice-width: 
-
-step through at intervals of 'slice-width', i.e. increase slice/crop coordinates 
-by 'slice-width' with each loop
-continue while inputImage1 and inputImage2 slice coordinates > smallest inputImage's width or height
-	(if slicing vertical bars, stop at smallest inputImage's width, if )
-
-'''
 
 def combine(img1, img2, sliceWidth, direction):
 	if direction == 'vertical':
@@ -23,41 +8,6 @@ def combine(img1, img2, sliceWidth, direction):
 	elif direction == 'horizontal':
 		return combineHorizontal(img1, img2, sliceWidth)
 
-
-
-def combineExpandedVertical(img1, img2, sliceWidth):
-	smallestHeight = smallestH(img1, img2)
-	smallestWidth = smallestW(img1, img2)
-
-	print(img1.size)
-	print(img2.size)	
-
-	#sliceLocation = sliceWidth
-	sliceStart = 0
-	sliceEnd = sliceWidth
-	slice = (sliceStart, 0, sliceEnd, smallestHeight)
-
-	img1PasteLocation = (0, 0)
-	img2PasteLocation = (sliceWidth, 0)
-
-	result = Image.new("RGB",(2*smallestWidth, smallestHeight))
-
-	while (slice[2] < (2*smallestWidth)):
-		img1_slice = img1.crop(slice)
-		img2_slice = img2.crop(slice)
-		result.paste(img1_slice, img1PasteLocation)
-
-		result.paste(img2_slice, img2PasteLocation)
-
-		img1PasteLocation = (img1PasteLocation[0]+(2*sliceWidth), 0)
-		img2PasteLocation = (img2PasteLocation[0]+(2*sliceWidth), 0)
-
-
-		sliceStart = slice[0] + sliceWidth
-		sliceEnd = slice[2] + sliceWidth
-		slice = (sliceStart, 0, sliceEnd, smallestHeight)
-
-	return result
 
 def combineVertical(img1, img2, sliceWidth):
 	smallestHeight = smallestH(img1, img2)
@@ -68,7 +18,6 @@ def combineVertical(img1, img2, sliceWidth):
 	slice = (sliceStart, 0, sliceEnd, smallestHeight)
 
 	pasteLocation = (0, 0)
-	#img2PasteLocation = (sliceWidth, 0)
 	pasteNext = 'img1'
 
 	result = Image.new("RGB",(smallestWidth, smallestHeight))
@@ -96,15 +45,12 @@ def combineHorizontal(img1, img2, sliceWidth):
 	smallestHeight = smallestH(img1, img2)
 	smallestWidth = smallestW(img1, img2)
 
-	#slice = (0, 0, smallestWidth, sliceWidth)	
 
-	#sliceLocation = sliceWidth
 	sliceStart = 0
 	sliceEnd = sliceWidth
 	slice = (0, sliceStart, smallestWidth, sliceEnd)
 
 	pasteLocation = (0, 0)
-	#img2PasteLocation = (sliceWidth, 0)
 	pasteNext = 'img1'
 
 	result = Image.new("RGB",(smallestWidth, smallestHeight))
